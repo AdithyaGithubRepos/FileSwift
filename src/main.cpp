95d395409772c6ef734f8360c1d01a293cd6b366
Adithya Github Work <adithya2011.work@gmail.com>
2024-06-22 15:21:26 +0530

    Errors included!

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,41 @@
 #include <cstdlib>
 #include "iostream"
 #include "string"
+#include "cstring"
+#include "fstream"
+#ifndef _WIN32
+#include "direct.h"
+#else
+#include "sys/stat.h"
+#include "sys/types.h"
+#endif
 
+int main(int argc, char* argv[])
+{
+    if(argc > 2 && std::strcmp(argv[2], "-mk")==0)
+    {
+      std::string filename = argv[2];
+      std::ofstream outfile(filename);
+      if(outfile.is_open())
+      {
+        std::cout << "File made successfully. Check parent directory.";
+        outfile.close();
+      } else
+      {
+        std::cerr << "Error creating file.\n";
+      }
+    } else if(std::strcmp(argv[1], "-mkd")==0)
+    {
+        std::string dirname = argv[2];
+#ifndef _WIN32 
+        if(_wmkdir(dirname.c_str())==0)
+#else
+        if(mkdir(dirname.c_str(), 0755)==0) 
+#endif            
+        
+        {
+
+        }    
+        
+    }
+}
